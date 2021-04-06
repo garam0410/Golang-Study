4a44d1e8a8b615f0273f92ffd807fba7b7e76d68
Garam Kim <0410garam@gmail.com>
2021-04-06 23:41:57 +0900

    Golang 변수 학습 수정

--- src/calculator/cal.go
+++ src/calculator/cal.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	//번수 선언 따로, 대입 따로
 	var a int
 	var b int
 	a = 3
 	b = 4
 
+	//타입 없이 대입
+	var d = 6
+	var e = 3.14
+
 	fmt.Println(a + b)
 
 	var word string = "abc"
 	fmt.Println(word)
+
+	fmt.Println(d)
+	fmt.Println(e)
 }
